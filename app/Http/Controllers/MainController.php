b2e3200a3d059783c104e85f9f861567dacbbeb5
Taofeek <taofeekolamilekan218@gmail.com>
2023-05-10 08:15:33 +0100

    Fix: qr

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,10 +15,6 @@ class MainController extends Controller
     public function create()
     {
         return view('welcome');
-        // return Inertia::render('Welcome', [
-        //     'status' => session('status'),
-        //     'users' => User::latest()->paginate(10),
-        // ]);
     }
 
     public function proccessed()
